2302151f78dbe66c561bea2cf410e101f20f35a9
joshpaulchan <joshpaulchan@gmail.com>
2016-06-29 06:30:23 -0400

    Add test for .gameStart

--- test.js
+++ test.js
@@ -9,9 +9,9 @@ describe("The Game Object", function() {
     });
     
     it("should create a new player object, add it to the player order, and return the id of the newly created player", function() {
-      expext(g.playerOrder.length).toBe(0);
+      expect(g.playerOrder.length).toBe(0);
       expect(g.addPlayer('Ethan')).toBe(jasmine.any(String));
-      expext(g.playerOrder.length).toBe(1);
+      expect(g.playerOrder.length).toBe(1);
     });
     
     it("should throw an error when trying to add a username of someone already playing", function() {
@@ -27,8 +27,49 @@ describe("The Game Object", function() {
   });
   
   decribe(".startGame", function() {
+    var g;
+    beforeEach(function() {
+      g = new Game();
+    });
     
-  })
+    it("should throw an error if there are less than two people playing", function() {
+      expect(g.playerOrder.length).toBe(0);
+      g.addPlayer('Ethan');
+      expect(g.startGame()).toThrow();
+    });
+    
+    it("should start the game only if more than two people have been added", function() {
+      expext(g.playerOrder.length).toBe(0);
+      g.addPlayer('Ethan');
+      g.addPlayer('Josh');
+      g.startGame();
+      expect(g.isStarted).toBe(true);
+    });
+  });
+  
+  describe(".isWinning", function() {
+    var g;
+    beforeEach(function() {
+      g = new Game();
+    });
+    
+  });
+  
+  describe(".playCard", function() {
+    var g;
+    beforeEach(function() {
+      g = new Game();
+    });
+    
+  });
+  
+  describe(".slap", function() {
+    var g;
+    beforeEach(function() {
+      g = new Game();
+    });
+    
+  });
 
   
 });
