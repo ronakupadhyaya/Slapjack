dfc8a6731e94110ba462a62f1868e2146b4a6202
Darwish <dargani123@gmail.com>
2016-06-29 08:52:15 -0400

    Update game.js

--- game.js
+++ game.js
@@ -34,11 +34,18 @@ var Game = function() {
   this.pile = [];
 };
 
+
+// Make sure the game is not started and the username is valid
+// Add Player to playerOlder
+// return player id 
 Game.prototype.addPlayer = function(username) {
 
 };
 
-Game.prototype.nextPlayer = function(playerId) {
+
+// Use this.playerOrder and this.currentPlayer to figure out whose turn it is next! 
+
+Game.prototype.nextPlayer = function() {
  
 };
 
@@ -52,14 +59,21 @@ Game.prototype.startGame = function() {
   
 };
 
+
+// Check if the player with playerId is winning. In this case, that means he has the whole deck.
 Game.prototype.isWinning = function(playerId) {
   
 };
 
+// Play a card from the end of the pile
 Game.prototype.playCard = function(playerId) {
   
 };
 
+
+// If there is valid slap, move all items of the pile into the players Pile,
+// clear the pile 
+// remember invalid slap and you should lose 3 cards!!
 Game.prototype.slap = function(playerId) {
   
 };
