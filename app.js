d36a0ff80397dab8fa8f9316195fa90c96cd0fbc
Mustafa Paksoy <mustafa@joinhorizons.com>
2016-06-28 21:17:34 -0400

    Add slapjack files.

---
+++ app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var path = require('path');
+var morgan = require('morgan');
+var path = require('path');
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(morgan('combined'));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Express started. Listening to port', port);
