4c9909fa958699f66cf0e7b3f30b274ebe5ff0ec
git status <tifchang@wharton.upenn.edu>
2017-06-27 13:29:22 -0700

    today

--- card.js
+++ card.js
@@ -1,10 +1,40 @@
 class Card {
   constructor(suit, value) {
-    // YOUR CODE HERE
+    this.suit = suit;
+    this.value = value;
   }
 
   toString() {
-    // YOUR CODE HERE
+    switch(this.value) {
+      case 1:
+        this.value = 'Ace';
+        break;
+      case 11:
+        this.value = 'Jack';
+        break;
+      case 12:
+        this.value = 'Queen';
+        break;
+      case 13:
+        this.value = 'King'
+        break;
+    }
+    switch(this.suit) {
+      case 'spades':
+        this.suit = 'Spades';
+        break;
+      case 'hearts':
+        this.suit = 'Hearts';
+        break;
+      case 'diamonds':
+        this.suit = 'Diamonds';
+        break;
+      case 'clubs':
+        this.suit = 'Clubs'
+        break;
+    }
+
+    return (this.value + " of " + this.suit)
   }
 
   // PERSISTENCE FUNCTIONS
