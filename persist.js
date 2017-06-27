a1dd2a3e8f9206c958b7b765a6ba69e7cd9e72b2
Jay <imjching@users.noreply.github.com>
2017-06-26 22:18:06 -0700

    complete all

--- persist.js
+++ persist.js
@@ -3,10 +3,10 @@ var STORE = './store.json'
 
 module.exports = {
   write: function(persist) {
-    fs.writeFileSync(STORE, persist, {encoding: 'utf8'});
+    fs.writeFileSync(STORE, persist, { encoding: 'utf8' });
   },
   read: function() {
-    return fs.readFileSync(STORE, {encoding: 'utf8'});
+    return fs.readFileSync(STORE, { encoding: 'utf8' });
   },
   hasExisting: function() {
     return fs.existsSync(STORE);
