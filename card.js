dcd898d75e21e88d71d7ae4f558278f905cc0342
Miguelangel De Armas <dearmasm@wharton.upenn.edu>
2017-07-13 14:37:03 -0700

    Constructed Card object and toString function

--- card.js
+++ card.js
@@ -1,10 +1,33 @@
 class Card {
   constructor(suit, value) {
     // YOUR CODE HERE
+    this.suit = suit
+    this.value = value
   }
 
   toString() {
     // YOUR CODE HERE
+    var string = this.suit.split("");
+    string[0] = string[0].toUpperCase();
+    var finalString = string.join("")
+
+    switch (this.value) {
+      case 1:
+        return "Ace of " + finalString
+        break;
+      case 11:
+        return "Jack of " + finalString
+        break;
+      case 12:
+        return "Queen of " + finalString
+        break;
+      case 13:
+        return "King of " + finalString
+        break;
+      default:
+        return this.value + " of " + finalString;
+        break;
+    }
   }
 
   // PERSISTENCE FUNCTIONS
