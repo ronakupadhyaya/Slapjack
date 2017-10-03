49795352f13374caeaddfa722d9d42563b5dcbf9
jeff <tangj1122@gmail.com>
2017-10-03 15:21:49 -0700

    fin step1

--- card.js
+++ card.js
@@ -1,10 +1,31 @@
 class Card {
   constructor(suit, value) {
     // YOUR CODE HERE
+    this.suit = suit;
+    this.value = value;
   }
 
   toString() {
     // YOUR CODE HERE
+    var str;
+
+    switch (this.value) {
+      case 1:
+        str = 'Ace';
+        break;
+      case 11:
+        str = 'Jack';
+        break;
+      case 12:
+        str = 'Queen';
+        break;
+      case 13:
+        str = 'King';
+        break;
+      default:
+        str = this.value;
+    }
+    return str + " of " + this.suit[0].toUpperCase() + this.suit.slice(1);
   }
 
   // PERSISTENCE FUNCTIONS
--- game.js
+++ game.js
@@ -6,31 +6,123 @@ var readGame = false;
 
 class Game {
   constructor() {
-    // YOUR CODE HERE
+    this.isStarted = false;
+    this.players = {};
+    this.playerOrder = [];
+    this.pile = [];
   }
 
   addPlayer(username) {
-    // YOUR CODE HERE
+    if (this.isStarted) {
+      throw "Game already started";
+    }
+    if (!username.trim()) {
+      throw "Username is empty";
+    }
+    if (this.usernameExists(username)) {
+      throw "User already exists";
+    }
+    var newPlayer = new Player(username);
+    this.playerOrder.push(newPlayer.id);
+    this.players[newPlayer.id] = newPlayer;
+    return newPlayer.id;
   }
 
   startGame() {
-    // YOUR CODE HERE
+    if (this.isStarted)
+      throw "Game already started";
+    if (Object.keys(this.players).length < 2)
+      throw "More than 1 player needed"
+    this.isStarted = true;
+    var suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
+    for (var s = 0; s < suits.length; s++) {
+      for (var i = 1; i < 14; i++) {
+        this.pile.push(new Card(suits[s], i));
+      }
+    }
+    _.shuffle(this.pile);
+
+    while (this.pile.length > 0)
+      for (var id in this.players)
+        if (this.pile.length)
+          this.players[id].pile.push(this.pile.pop());
   }
 
   nextPlayer() {
-    // YOUR CODE HERE
+    if (!this.isStarted)
+      throw "Game has not yet started";
+    this.playerOrder.push(this.playerOrder.shift());
   }
 
   isWinning(playerId) {
-    // YOUR CODE HERE
+    if (!this.isStarted)
+      throw "Game has not yet started";
+    if (this.players[playerId].pile.length === 52) {
+      this.isStarted = false;
+      return true;
+    }
+    return false;
   }
 
   playCard(playerId) {
-    // YOUR CODE HERE
+    if (!this.isStarted)
+      throw "Game has not yet started";
+    if (playerId !== this.playerOrder[0])
+      throw "Not this player's turn.";
+    if (!this.players[playerId].pile.length)
+      throw "This player is pileless!";
+    var newCard = this.players[playerId].pile.pop();
+    this.pile.push(newCard);
+
+    var counter = 0;
+    for (var id in this.players) {
+      if (this.players[id].pile.length === 0)
+        counter++
+    }
+    if (counter === Object.keys(this.players).length) {
+      this.isStarted = false;
+      throw "Issa tie";
+    }
+    this.nextPlayer();
+    return {
+      card: newCard,
+      cardString: newCard.toString()
+    }
   }
 
   slap(playerId) {
-    // YOUR CODE HERE
+    if (!this.isStarted)
+      throw "Game has not yet started";
+    var topThree = this.pile.slice(-3);
+    var len = topThree.length;
+    if (topThree[len - 1].value === 11 ||
+      (len >= 2 && topThree[len - 2].value === topThree[len - 1].value) ||
+      (len >= 3 && topThree[len - 3].value === topThree[len - 1].value)) {
+        this.players[playerId].pile = [...this.pile, ...this.players[playerId].pile];
+        this.pile = [];
+        return {
+          winning: this.isWinning(playerId),
+          message: 'got the pile!'
+        }
+      }
+    var cardsToTake = this.players[playerId].pile.length < 3 ? this.players[playerId].pile.length : 3;
+    for (var i = 0; i < cardsToTake; i++) {
+      this.pile.unshift(this.players[playerId].pile.pop());
+    }
+    console.log("\nLEN", this.pile);
+    return {
+      winning: false,
+      message: 'lost 3 cards!'
+    }
+  }
+
+  usernameExists(username) {
+    for (var id in this.players) {
+      if (this.players[id].username === username) {
+        return true;
+      }
+    }
+    return false
   }
 
   // PERSISTENCE FUNCTIONS
